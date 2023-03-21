414e167f410035845964d0e585f506561d826eff
Meypod <radical.zero@hotmail.com>
2023-03-21 07:57:38 +0330

    fix: add a normal date for comparison

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ const calcParams = new CalculationParameters("Tehran");
 
 const specialDate1 = new Date(1679257800000);
 const specialDate2 = new Date(1679344200000);
+const normalDate3 = new Date(1678480200000);
 
 const prayerTimes1 = new PrayerTimes(coords, specialDate1, calcParams);
 const sunnah1 = new SunnahTimes(prayerTimes1);
@@ -19,6 +20,11 @@ const sunnah2 = new SunnahTimes(prayerTimes2);
 prayerTimes2.middleOfTheNight = sunnah2.middleOfTheNight;
 prayerTimes2.lastThirdOfTheNight = sunnah2.lastThirdOfTheNight;
 
+const prayerTimes3 = new PrayerTimes(coords, normalDate3, calcParams);
+const sunnah3 = new SunnahTimes(prayerTimes3);
+prayerTimes3.middleOfTheNight = sunnah3.middleOfTheNight;
+prayerTimes3.lastThirdOfTheNight = sunnah3.lastThirdOfTheNight;
+
 const renderItem = ({item}) => {
   return <Text>{prayerTimes1[item].toString()}</Text>
 };
@@ -27,13 +33,18 @@ const renderItem2 = ({item}) => {
   return <Text>{prayerTimes2[item].toString()}</Text>
 };
 
+const renderItem3 = ({item}) => {
+  return <Text>{prayerTimes3[item].toString()}</Text>
+};
+
 
 export default function App() {
 
   return (
     <View style={styles.container}>
-      <FlatList data={prayers} renderItem={renderItem} style={{marginBottom: 30}}/>
-      <FlatList data={prayers} renderItem={renderItem2} style={{marginBottom: 30}}/>
+      <FlatList data={prayers} renderItem={renderItem} style={{marginBottom: 10}}/>
+      <FlatList data={prayers} renderItem={renderItem2} style={{marginBottom: 10}}/>
+      <FlatList data={prayers} renderItem={renderItem3}/>
     </View>
   );
 }
